1eaed068e268b8dc39803ecf900f28d543be6acf
Matt Jarrett <cujarrett@gmail.com>
2018-06-24 22:17:13 -0500

    Adds simple demo GUI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
 import "./App.css"
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries } from "react-vis"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      /* eslint-disable id-length */
+      <XYPlot
+        width={300}
+        height={300}>
+        <HorizontalGridLines />
+        <LineSeries
+          data={[
+            { x: 1, y: 10 },
+            { x: 2, y: 5 },
+            { x: 3, y: 15 }
+          ]}/>
+        <XAxis />
+        <YAxis />
+      </XYPlot>
     )
+    /* eslint-enable id-length */
   }
 }
 
