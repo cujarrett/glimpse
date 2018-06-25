765976ec8f2080f2f6d9dba97ae4953ffc2911e2
Matt Jarrett <cujarrett@gmail.com>
2018-06-24 22:17:19 -0500

    Removes console log

--- src/services/github.js
+++ src/services/github.js
@@ -4,7 +4,7 @@ const getGitHubUserData = async (userName) => {
   const url = `https://github-contributions-api.now.sh/v1/${userName}`
   const response = await fetch(url)
   const json = await response.json()
-  console.log(json)
+  return json
 }
 
 getGitHubUserData("matt-jarrett")
