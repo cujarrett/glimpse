0417f27130c37487ef426fcbb701f089e79c825a
Matt Jarrett <cujarrett@gmail.com>
2018-07-02 23:08:37 -0500

    Exports api handler to app

--- src/services/github.js
+++ src/services/github.js
@@ -1,10 +1,8 @@
 const fetch = require("node-fetch")
 
-const getGitHubUserData = async (userName) => {
+export const getGitHubUserData = async (userName) => {
   const url = `https://github-contributions-api.now.sh/v1/${userName}`
   const response = await fetch(url)
   const json = await response.json()
   return json
 }
-
-getGitHubUserData("matt-jarrett")
