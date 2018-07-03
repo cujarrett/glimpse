801d6f84d689a61f2d9c4343dfa110fa5733b895
Matt Jarrett <cujarrett@gmail.com>
2018-07-02 23:08:10 -0500

    Updates grpah library

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React, { Component } from "react"
 import "./App.css"
-import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries } from "react-vis"
+import { getGitHubUserData } from "./services/github"
+// Disable eslint max-len for imports from react-vis
+// eslint-disable-next-line max-len
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, VerticalBarSeries } from "react-vis"
 
 class App extends Component {
+  async componentDidMount() {
+    const formattedData = []
+    const data = await getGitHubUserData("matt-jarrett")
+    const contributions = data.contributions.reverse()
+
+    for (const contribution of contributions) {
+      // Disable eslint id-length as variable names x and y are normal axis names
+      // eslint-disable-next-line id-length
+      formattedData.push({ x: contribution.date, y: contribution.count })
+    }
+
+    this.setState({ formattedData })
+  }
+
   render() {
+    const BarSeries = VerticalBarSeries
+
     return (
-      /* eslint-disable id-length */
-      <XYPlot
-        width={300}
-        height={300}>
-        <HorizontalGridLines />
-        <LineSeries
-          data={[
-            { x: 1, y: 10 },
-            { x: 2, y: 5 },
-            { x: 3, y: 15 }
-          ]}/>
-        <XAxis />
-        <YAxis />
-      </XYPlot>
+      <div>
+        <div align="center">
+          <h1>{"GitHub Contributions"}</h1>
+        </div>
+        { this.state && this.state.formattedData &&
+      <div>
+        <XYPlot
+          xType="ordinal"
+          width={1200}
+          height={500}
+        >
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis />
+          <YAxis />
+          <BarSeries
+            className="vertical-bar-series-example"
+            data={ this.state.formattedData }/>
+        </XYPlot>
+      </div>
+        }
+      </div>
     )
-    /* eslint-enable id-length */
   }
 }
 
