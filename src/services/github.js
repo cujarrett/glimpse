abe54a44ba986c4a7e3bfba444fc4fc84a7804a4
Matt Jarrett <cujarrett@gmail.com>
2018-07-07 05:40:47 -0500

    Cleans data before it's returned

--- src/services/github.js
+++ src/services/github.js
@@ -1,8 +1,25 @@
 const fetch = require("node-fetch")
+const uniqBy = require("lodash/uniqBy")
 
 export const getGitHubUserData = async (userName) => {
   const url = `https://github-contributions-api.now.sh/v1/${userName}`
   const response = await fetch(url)
-  const json = await response.json()
-  return json
+  let { contributions } = await response.json()
+  contributions = contributions.reverse()
+
+  const unique = uniqBy(contributions, "date")
+  contributions = unique
+
+  const correctedInitialDate = []
+  let initialDateFound = false
+  for (const contribution of contributions) {
+    if (initialDateFound || contribution.count > 0) {
+      correctedInitialDate.push(contribution)
+      initialDateFound = true
+    }
+  }
+
+  contributions = correctedInitialDate
+
+  return contributions
 }
