5935b36d3e24f2a637f0bd23bdd1f99020eb82a5
Matt Jarrett <cujarrett@gmail.com>
2018-07-13 00:09:48 -0500

    Imports babel-polyfill to top of imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import "babel-polyfill"
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 import "font-awesome/css/font-awesome.min.css"
-import "babel-polyfill"
 
 ReactDOM.render(<App />, document.getElementById("root"))
 registerServiceWorker()
