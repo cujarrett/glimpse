b35eab11dcfba072fe6bdf7f01a0cb45ca82f277
Matt Jarrett <cujarrett@gmail.com>
2018-07-12 23:40:32 -0500

    Adds babel-polyfill to fix IE bug

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 import "font-awesome/css/font-awesome.min.css"
+import "babel-polyfill"
 
 ReactDOM.render(<App />, document.getElementById("root"))
 registerServiceWorker()
