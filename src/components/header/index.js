57f5d101d3d82875847542351550a6d4259febe9
Matt Jarrett <cujarrett@gmail.com>
2019-12-07 22:55:17 -0600

    Updates source code link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,9 +6,6 @@ import logo from "./glimpse-logo.png"
 export const Header = (props) => {
   return (
     <header>
-      <div className="github-link">
-        <a href="https://github.com/cujarrett/glimpse"><i className="fa fa-star"/> on GitHub</a>
-      </div>
       <div className="app-header">
         <a href="https://glimpse.ninja"><img src={logo} className={props.logoStyling} alt="logo" /></a>
       </div>
