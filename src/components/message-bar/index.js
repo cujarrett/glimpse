4a845ced9a1f5be1a8564e294d1ab3a519b044e7
Matt Jarrett <cujarrett@gmail.com>
2020-02-23 10:39:25 -0600

    refactor: message bar logic

--- src/components/message-bar/index.js
+++ src/components/message-bar/index.js
@@ -5,9 +5,12 @@ import "./style.css"
 export const MessageBar = (props) => {
   if (props.contributions.length > 0) {
     const userGitHubUrl = `https://github.com/${props.input}`
+    const startOfMessage = props.message.substring(0, props.message.indexOf("at") + 3)
+    const endOfMessage = props.message.substring(props.message.indexOf("GitHub") - 3)
+    const userName = props.input
     return (
       <message-bar>
-        <h4>A glimpse at <a href={userGitHubUrl} target="_blank">{props.input}</a>'s GitHub contributions</h4>
+        <h4>{startOfMessage}<a href={userGitHubUrl} target="_blank" rel="noopener noreferrer">{userName}</a>{endOfMessage}</h4>
       </message-bar>
     )
   } else {
