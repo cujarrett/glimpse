b12daa96968862e7efd1994122cb111ad474312b
Matt Jarrett <cujarrett@gmail.com>
2020-04-25 13:11:55 -0500

    :white_check_mark: Updating Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { fireEvent, render } from "@testing-library/react"
 import App from "./App"
 
-it("renders without crashing", () => {
-  const div = document.createElement("div")
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+test("renders initial message", () => {
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/Search any GitHub username for a glimpse at their open source contributions/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+test("demo works as expected", async () => {
+  const { getByText } = render(<App />)
+  // Click button
+  fireEvent.click(getByText(/Show me a demo/i))
+
+  // Wait for page to update with query text
+  const items = await findByText(node, /Item #[0-9]: /)
+  expect(items).toHaveLength(10)
 })
