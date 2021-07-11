496ca05bd4c98905fa2f66d02ec2ac5c9fc96882
Matt Jarrett <cujarrett@gmail.com>
2021-07-11 03:11:44 -0500

    feat: removes version in footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import "./style.css"
-import { version } from "../../../package.json"
 
 export const Footer = (props) => {
   return (
@@ -13,9 +12,6 @@ export const Footer = (props) => {
         </a>{" "}
         with <i className="fa fa-heart" /> and JavaScript
       </h4>
-      <div className="version">
-        <a href="https://github.com/cujarrett/glimpse/blob/main/CHANGELOG.md">{version}</a>
-      </div>
     </div>
   )
 }
