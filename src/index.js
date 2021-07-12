bf9fc9b3551e5c9cf5b3f77922a8b88527471124
Matt Jarrett <16245634+cujarrett@users.noreply.github.com>
2021-07-11 21:52:57 -0500

    feat: removes service worker (#142)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import Glimpse from "./App.js"
-import registerServiceWorker from "./registerServiceWorker"
+import reportWebVitals from "./reportWebVitals"
 import "font-awesome/css/font-awesome.min.css"
 
 ReactDOM.render(<Glimpse />, document.getElementById("root"))
-registerServiceWorker()
+reportWebVitals()
