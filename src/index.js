dd94a8260a584db16679144c9b62b51ea7b56a34
Matt jarrett <cujarrett@gmail.com>
2021-12-26 14:11:21 -0600

    chore: renames report-web-vitals

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import Glimpse from "./App.js"
-import reportWebVitals from "./reportWebVitals"
+import reportWebVitals from "./report-web-vitals.js"
 import "font-awesome/css/font-awesome.min.css"
 
 ReactDOM.render(
